a97d85ae81d7e3ad730b756814cc600224120141
zjun <565061763@qq.com>
2021-05-26 00:45:12 +0800

    FMediaFileProvider

--- lib/src/main/java/com/sd/lib/media_store/uri/FMediaFileProvider.kt
+++ lib/src/main/java/com/sd/lib/media_store/uri/FMediaFileProvider.kt
@@ -1,13 +1,26 @@
 package com.sd.lib.media_store.uri
 
 import android.content.Context
+import android.net.Uri
+import android.os.Build
 import androidx.core.content.FileProvider
+import java.io.File
 
 class FMediaFileProvider : FileProvider() {
     companion object {
         @JvmStatic
         fun getAuthority(context: Context): String {
             return "${context.packageName}.${FMediaFileProvider::class.java.simpleName.toLowerCase()}"
+        }
+
+        @JvmStatic
+        fun fileToUri(file: File?, context: Context): Uri? {
+            if (file == null) return null
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                getUriForFile(context, getAuthority(context), file)
+            } else {
+                Uri.fromFile(file)
+            }
         }
     }
 }
